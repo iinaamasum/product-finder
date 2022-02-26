fa48e4a2cdff2aa94f31c3916b311ae6effe17f6
iinaamasum <iinaamasum@gmail.com>
2022-02-27 01:49:46 +0600

    done__done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const getProduct = (id) => {
-  let url = '';
+  let url = `https://fakestoreapi.com/products`;
   if (id == 'jewe') {
     url = `https://fakestoreapi.com/products/category/jewelery`;
   } else if (id == 'elec') {
@@ -15,7 +15,7 @@ const getProduct = (id) => {
     .then((res) => res.json())
     .then((data) => updateCard(data));
 };
-
+getProduct(true);
 const updateCard = (data) => {
   console.log(data);
   document.getElementById('api__card__row').textContent = '';
@@ -23,10 +23,10 @@ const updateCard = (data) => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-        <div class="card h-100">
-          <img height='350' src="${info.image}" class="card-img-top" alt="...">
+    <div class="card h-100 shadow">
+    <img height='350' src="${info.image}" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title text-primary">${info.title}</h5>
+          <h5 class="card-title text-primary">${info.title}</h5>
             <p class="card-text">${info.description.slice(0, 200)}...</p>
           </div>
           <div class="card-footer bg-white">
@@ -38,17 +38,38 @@ const updateCard = (data) => {
               </div>
               <div class="col">
                 <button class="col-12 btn btn-outline-success">Buy Now</button>
-              </div>
-            </div>
+                </div>
+                </div>
             <div class="d-flex justify-content-between">
               <small class="text-muted">Id: ${info.id}</small>
               <small class="text-muted">Price: ${info.price}</small>
               <small class="text-muted">Category: ${info.category}</small>
-            </div>
-          </div>
-        </div>
-  `;
+              </div>
+              </div>
+              </div>
+              `;
     document.getElementById('api__card__row').appendChild(div);
   });
+  document.getElementById('carousel__inner0').textContent = '';
+  document.getElementById('carousel__inner1').textContent = '';
+  document.getElementById('carousel__inner2').textContent = '';
+  for (let i = 0; i < 3; i++) {
+    carouselShow(data[i], i);
+  }
 };
-getProduct('electronics');
+
+/* carosel */
+
+const carouselShow = (info, id) => {
+  const div2 = document.createElement('div');
+  // div2.classList.add('carousel-item');
+  div2.innerHTML = `
+    <img height="732" src="${info.image}" class="d-block w-100" alt="...">
+    <div class="carousel-caption">
+      <h5>${info.title}</h5>
+      <p>${info.description.slice(0, 40)}</p>
+    </div>
+    `;
+  console.log(div2);
+  document.getElementById('carousel__inner' + id).appendChild(div2);
+};
